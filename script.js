37a75e214a243f2e35020d867d37f119330a5c5b
sepkeuchenius <septimber@gmail.com>
2019-12-17 20:19:44 +0100

    Login done

--- script.js
+++ script.js
@@ -1,181 +1,54 @@
-//TODO
-//add users
-//user search function
-//history keeping
-//reset button
-//redesign bill
-//manual buttons
-
-
-//uservars
-var snackbarContainer = document.querySelector('#demo-snackbar-example');
-var activeUser = [];
-var currentUser = '';
-var products = []; //Firebase Database
-var users = []; //Firebase Database
-var productNames = []; //Firebase Database
-var productPrices = []; //Firebase Database
-var del = 1;
-var mult = 1;
-var loader = true;
-
-
-
-//database variables
 var database = firebase.database();
-database.ref('Products/').once('value').then(function(snapshot){
-  var data =  snapshot.val();
-  products = data
-  console.log(products)
-})
-database.ref('Product_names/').once('value').then(function(snapshot){
-  productNames = snapshot.val()
-})
-database.ref('Product_prices/').once('value').then(function(snapshot){
-  productPrices = snapshot.val()
-})
 
 
-//button functions
-function multiplier(el){
-  if(mult == Number(el.attr('id').substring(1))){
-    mult = 1
-    el.css('box-shadow', 'none')
-  }
-  else{
-    mult = Number(el.attr('id').substring(1))
-    el.css('box-shadow', '0 0 20px red')
-  }
-}
-function deleter(){
-  if(del == 1){
-    del =-1
-    $('#kassa').css('background', 'rgb(253, 114, 114)')
-  }
-  else{
-    del = 1
-    $('#kassa').css('background', '#ebebeb')
-  }
-}
 
-//loader function
-function load(){
-  if(loader){
-    loader = false;
-    $('#p2').hide()
-  }
-  else{
-    load = true;
-    $('#p2').show()
-  }
-}
+$('#login_button').on('click', function(){user_login()})
 
+function user_login(){
+  var username= $('#username').val()
+  var pwd = $('#password').val()
+  console.log(username)
+  var userNumber = 0
+   database.ref('users').once('value').then(function(snapshot){
+      var data =  snapshot.val();
+      // var copy_of_data = data
+      data = Object.keys(data)
+      for(var i in data){
+        // console.log(data[i])
+        if(username == data[i]){
+          console.log(i)
+          userNumber = Number(i);
 
-//load users
-database.ref('users').once('value').then(function(snapshot){
-  var data =  snapshot.val();
-  users = Object.keys(data)
-  $.each(users, function(i,x){
-    //x is user name
-    // $item = $('<a class="mdl-navigation__link user" id="a_' + x + '">' + x+  ' <i class="material-icons" style="vertical-align: middle; float: right;" onclick="removeUser($(this))">delete</i></a>')
-    // $('.mdl-navigation').append($item)
-  })
-  load()
-
-  $('.user').on('click', function(){
-    currentUser = $(this).attr('id').substring(2)
-    $('#usertitle').html(' &rarr; ' + currentUser)
-
-    loadBill()
-    $(this).detach().prependTo('.mdl-navigation')
-  })
-})
-
-//reload billlist
-function loadBill(){
-  database.ref('users/' + currentUser).once('value').then(function(snapshot){
-    var data =  snapshot.val();
-    $('#billList').empty()
-    var due = 0;
-    for(var i in data){
-      if(data[i] > 0){
-        $('#billList').append('<li class="mdl-list__item"> ' + productNames[i] + ' : ' + data[i])
-        due += data[i] * productPrices[i]
-      }
-    }
-    $('#due-amount').text(due.toFixed(2))
-  })
-}
-
-//button clicks
-$('.product').on('click', function(){
-  var product = $(this).attr('id')
-
-//find i for the addition
-  for(var i in products){
-    if(product == products[i]){
-      console.log(products[i])
-      break;
-    }
-  }
-  database.ref('users/' + currentUser).once('value').then(function(snapshot){
-    var userdata = snapshot.val()
-    // var userdata = data.currentUser
-    var msg = mult +' '+ products[i] + ' toegevoegd'
-    userdata[i] = userdata[i] + 1 * del * mult;
-    if(del == -1){
-        msg = mult +' '+ products[i] + ' verwijderd'
-    deleter()
-  }
-
-  var data = {
-     message: msg,
-     timeout: 3000,
-     actionText: 'Undo'
-   };
-   snackbarContainer.MaterialSnackbar.showSnackbar(data);
-  if(mult != 1){
-    mult = 1;
-    $('.multi').css('box-shadow', 'none')
-  }
-    console.log(userdata)
-    database.ref('users/' + currentUser).set(userdata,
-       function(error){
-        if(error){
-          alert('wow, niet gelukt!')
-        }
-        else{
-          loadBill()
-        // productSucces()
         }
       }
 
-    );
-  })
+      if(userNumber){
+       database.ref('passwords').once('value').then(function(snapshot){
+            var data =  snapshot.val();
+            console.log(data)
+            if(data[Number(userNumber)] == pwd ){
+              // console.log('yes')
+              loadUser(username)
+              return;
+            }
+              alert('Foute wachtwoord')
+          });
 
-})
+      }
+      else{
+      alert('Gebruiker bestaat niet')
+    }
+  });
 
-
-function clearCart(){
-  if(currentUser.length < 1 || prompt('Code') != 5831 || !confirm('Zeker weten?')){
-    alert('Afgebroken')
-    return;
-  }
-  var newar = [];
-  for(var i in products){
-    newar.push(0)
-  }
-  database.ref('users/' + currentUser).set(newar);
-  loadBill()
 
 }
-
-function removeUser(el){
-  if(prompt('Code') != 5831 || !confirm('Zeker weten?')){
-    alert('Afgebroken')
-    return;
-  }
-el.parent().remove()
+function loadUser(user){
+  $('#login_page').hide()
+  $('#user_home').fadeIn()
+   database.ref('users').child(user).once('value').then(function(snapshot){
+     var data = snapshot.val()
 
 
-} //just removing the element for now, database comes later
+  })
+
+}
